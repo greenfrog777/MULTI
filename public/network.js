1651ca4cf719e1e41ac24e57851d9d8fd28f0c51
unknown <pghartley@gmail.com>
2025-08-23 16:26:12 +0100

    Move to using socket.io, should be simpler

--- public/network.js
+++ public/network.js
@@ -1,29 +1,30 @@
-let ws, myId, players = {}, colours = {};
+let socket, myId, players = {}, colours = {};
 
 function connectToServer(onInit, onUpdate, onRemove) {
-    ws = new WebSocket(`ws://${window.location.host}`);
+    socket = io();
 
-    ws.onmessage = (event) => {
-        const data = JSON.parse(event.data);
-        if (data.type === 'init') {
-            myId = data.myId;
-            for (let id in data.players) {
-                colours[id] = id === myId ? 0x0000ff : Phaser.Display.Color.RandomRGB().color;
-            }
-            onInit(data.players);
-        } else if (data.type === 'update') {
-            if (!players[data.id]) {
-                colours[data.id] = data.id === myId ? 0x0000ff : Phaser.Display.Color.RandomRGB().color;
-            }
-            onUpdate(data.id, data.position);
-        } else if (data.type === 'remove') {
-            onRemove(data.id);
+    socket.on('init', data => {
+        myId = data.myId;
+        for (let id in data.players) {
+            colours[id] = id === myId ? 0x0000ff : Phaser.Display.Color.RandomRGB().color;
         }
-    };
+        onInit(data.players);
+    });
+
+    socket.on('update', data => {
+        if (!players[data.id]) {
+            colours[data.id] = data.id === myId ? 0x0000ff : Phaser.Display.Color.RandomRGB().color;
+        }
+        onUpdate(data.id, data.position);
+    });
+
+    socket.on('remove', id => {
+        onRemove(id);
+    });
 }
 
 function sendMove(position) {
-    if (ws && ws.readyState === WebSocket.OPEN) {
-        ws.send(JSON.stringify({ type: 'move', position }));
+    if (socket && socket.connected) {
+        socket.emit('move', position);
     }
 }
--- server.js
+++ server.js
@@ -1,43 +1,30 @@
 const express = require('express');
-const WebSocket = require('ws');
 const http = require('http');
+const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const io = new Server(server);
 
 app.use(express.static('public'));
 
 let players = {};
 
-
-wss.on('connection', ws => {
-    const id = Date.now();
+io.on('connection', socket => {
+    const id = socket.id;
     players[id] = { x: 400, y: 300 };
 
     // Send current players to the new client, including their own ID
-    ws.send(JSON.stringify({ type: 'init', players, myId: id }));
+    socket.emit('init', { players, myId: id });
 
-    ws.on('message', message => {
-        const data = JSON.parse(message);
-
-        if (data.type === 'move') {
-            players[id] = data.position;
-            wss.clients.forEach(client => {
-                if (client.readyState === WebSocket.OPEN) {
-                    client.send(JSON.stringify({ type: 'update', id, position: data.position }));
-                }
-            });
-        }
+    socket.on('move', position => {
+        players[id] = position;
+        io.emit('update', { id, position }); // broadcast to all clients
     });
 
-    ws.on('close', () => {
+    socket.on('disconnect', () => {
         delete players[id];
-        wss.clients.forEach(client => {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(JSON.stringify({ type: 'remove', id }));
-            }
-        });
+        io.emit('remove', { id }); // broadcast to all clients
     });
 });
 
